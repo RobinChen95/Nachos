825f24ae8432dd3f094b845167ee4479c8e75180
RobinChen95 <407339708@pku.edu.cn>
2019-05-03 09:21:25 +0000

    2019-05-03 09:21:25 updated

---
+++ depoly.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+
+git config --global credential.helper store
+
+echo "git add ."
+git add .
+
+echo "acquiring time now"
+time=$(date "+%Y-%m-%d %H:%M:%S")
+echo "now is $time"
+
+echo "git commit $time updated"
+git commit -m "$time updated"
+
+echo "git push origin master"
+git push origin master
